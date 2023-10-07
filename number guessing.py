deedbd4833df8020fc17263dc3e2314e9b7d50ae
Aaditya Raj <aadityaraj085@gmail.com>
2023-10-07 19:04:47 +0530

    GUI using Tkinter
    Added GUI using Tkinter

--- number guessing.py
+++ number guessing.py
@@ -1,4 +1,28 @@
 import random
+import tkinter as tk
+from tkinter import messagebox
+
+def check_guess():
+    global guesses
+    user_guess = guess_entry.get()
+    guess_entry.delete(0, 'end')
+
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+        if user_guess == random_number:
+            messagebox.showinfo("Congratulations", f"Yess!! You got it in {guesses} guesses!")
+            window.quit()
+        
+        elif user_guess > random_number:
+            messagebox.showinfo("Guess Result", "You were above the number")
+            guesses += 1
+                
+        else:
+            messagebox.showinfo("Guess Result", "You were below the number")
+            guesses += 1
+    else:
+        messagebox.showinfo("Invalid Input", "Please type a number.")
 
 seed = int(input("Enter a random number (integer): "))
 random.seed(seed)
@@ -16,26 +40,18 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
-
 guesses = 0
 
-while True:
-    guesses += 1
-    user_guess = input("Please Make a guess: ")
-    
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    
-        if user_guess == random_number:
-            print("Yess!! You got it!")
-            break 
-        
-        elif user_guess > random_number:
-            print("You were above the number")
-                
-        else:
-            print("You were below the number")
-    else:
-        print("Please type a number.")
-        
-print("Congratulations!! You got it", str(guesses), "guesses")
+window = tk.Tk()
+window.title("Number Guessing Game")
+
+label = tk.Label(window, text="Guess the number:")
+label.pack(pady=10)
+
+guess_entry = tk.Entry(window, width=10)
+guess_entry.pack()
+
+guess_button = tk.Button(window, text="Submit Guess", command=check_guess)
+guess_button.pack(pady=10)
+
+window.mainloop()
